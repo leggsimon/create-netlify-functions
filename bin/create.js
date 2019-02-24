7e6fce2b715f24b66c0113e6010435d7dae647fb
Simon Legg <leggsimon@gmail.com>
2019-02-24 18:31:53 +0000

    exit with error if targetDirectory already exists

--- bin/create.js
+++ bin/create.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
 'use strict';
 
+const argv = require('yargs').argv;
+
+const logRed = (...args) => console.log('\x1b[31m', ...args, '\x1b[0m');
+
+const targetDirectory = argv._[0];
+
+if (!targetDirectory) {
+	logRed('No target directory was specified.');
+	process.exit(1);
+}
+
 process.exit(0);
