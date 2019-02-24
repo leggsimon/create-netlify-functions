82776feae523e4ec77be66b37a06cdf55480b542
Simon Legg <leggsimon@gmail.com>
2019-02-24 20:15:43 +0000

    refactor create.js

--- bin/create.js
+++ bin/create.js
@@ -5,6 +5,7 @@ const argv = require('yargs').argv;
 const fse = require('fs-extra');
 
 const logRed = (...args) => console.log('\x1b[31m', ...args, '\x1b[0m');
+const logGreen = (...args) => console.log('\x1b[32m', '✓', ...args, '\x1b[0m');
 
 const targetDirectory = argv._[0];
 
@@ -13,25 +14,42 @@ if (!targetDirectory) {
 	process.exit(1);
 }
 
-fse.pathExists(targetDirectory)
-	.then(exists => {
-		if (exists) {
-			logRed(
-				`${targetDirectory} already exists, please choose another destination`,
-			);
-			process.exit(1);
-		} else {
-			return fse.mkdirp(targetDirectory);
-		}
-	})
-	.then(() => {
-		console.log(`created ${targetDirectory}`);
-		return fse.copy(`${__dirname}/../examples/default`, targetDirectory);
-	})
-	.then(() => {
-		process.exit(0);
-	})
+async function checkDirectory(target) {
+	const exists = await fse.pathExists(targetDirectory);
+
+	if (exists) {
+		throw new Error(`The directory ${target} already exists.`);
+	}
+
+	return target;
+}
+
+async function createTargetDirectory(target) {
+	try {
+		await fse.mkdirp(target);
+		logGreen(`Successfully created ${target}`);
+		return target;
+	} catch (error) {
+		throw new Error(`An error occured when creating ${target}`);
+	}
+}
+
+async function copyFilesToTarget(target) {
+	try {
+		const exampleDirectory = `${__dirname}/../examples/default`;
+		await fse.copy(exampleDirectory, targetDirectory);
+		logGreen(`Successfully copied files to ${target}`);
+		return;
+	} catch (error) {
+		throw new Error(`An error occured when copying the files.`);
+	}
+}
+
+checkDirectory(targetDirectory)
+	.then(createTargetDirectory)
+	.then(copyFilesToTarget)
+	.then(() => process.exit(0))
 	.catch(error => {
-		logRed(`Error creating ${targetDirectory}`, error);
+		logRed('\n', error, '\n');
 		process.exit(1);
 	});
